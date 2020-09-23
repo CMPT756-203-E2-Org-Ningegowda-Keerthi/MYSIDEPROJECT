4e9b8b97af216b44b0d6068f3905d14c21fd9b0c
KeerthiNingegowda <keerthi.n.gowda11@gmail.com>
2020-09-23 11:26:28 -0700

    Adding a file that adds 2 integers and returns their sum

---
+++ sum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+##Author - Keerthi Ningegowda
+#This program demonstrates addition of 2 numbers
+
+print("This program performs addition of 2 numbers")
+
+##Taking input from user
+a = int(input("Enter first number  "))
+b = int(input("Enter second number "))
+
+
+##Print sum of 2 numbers
+print("The sum of two numbers is ", a + b)
+
